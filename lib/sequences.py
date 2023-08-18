d693197b99a92ea954bf8b7099d4b94011313150
user <mutukukennedy34@gmail.com>
2023-08-18 10:13:20 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+ if length<= 0 :
+   return []
+ elif length == 1 :
+   return [0]
+ elif length == 2:
+   return [0 , 1]
+ else :
+   seq = [0 , 1]
+   for i in range (2 , length):
+     next_num = seq[i -1] + seq[i-2]
+     seq.append(next_num)
+ print(seq)
+   
